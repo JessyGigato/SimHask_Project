bb6b793ae1d5a652ba3cfe8f3bc0695739110732
JessyGigato <jessygigato@gmail.com>
2022-02-07 17:13:57 -0500

    working on robot

--- agents.hs
+++ agents.hs
@@ -1,21 +1,7 @@
 module Agents where
 import Utils
 
-------------------------------------------------------------------------------------------
-----                                       Niño                                       ----
-------------------------------------------------------------------------------------------
-
-------------------------- METODOS AUXILIARES DE NIñOS -----------------
-
--- buscar pos del niño
-search_child_dir :: Pos -> [Child] -> Int
-search_child_dir pos childs = snd (child_pos_search pos childs)
-
--- buscar niño por posicion
-child_pos_search :: Pos -> [Child] -> Child
-child_pos_search _ [] = ((-1, -1), -1)
-child_pos_search pos (child: xr) | pos == (fst child) = child
-                                  | otherwise = child_pos_search pos xr
+------------------------- METODOS AUXILIARES -----------------
 
 -- pos a la que me voy a mover
 is_next_obj :: Pos -> Int -> [Pos]  -> Bool
@@ -44,22 +30,62 @@ is_not_bbppen pos dir list = notElem (mov_dir pos dir) list
 ------------------------------- METODOS DE MOVIMIENTO ----------------------
 
 -- lista de objetos actualizada
-mov_obj :: Pos -> Pos -> [Pos] -> [Pos]
-mov_obj p1 p2 objects = concat_list p2 (list_without_elems p1 objects)
+move_object__ :: Pos -> Pos -> [Pos] -> [Pos]
+move_object__ p1 p2 objects = p2 : (list_without_elems p1 objects)
 
-move_object :: Pos -> [Pos] -> [Pos] -> Int -> Int -> Int -> (Pos, [Pos])
-move_object pos objects childs dir n m = if in_range (get_surr_by_dir pos dir objects) && is_not_dirt (get_surr_by_dir pos dir objects) && is_not_robot ((get_surr_by_dir pos dir objects))
-                                then ((mov_dir pos dir), ( mov_obj (mov_dir pos dir) (get_surr_by_dir pos dir objects)))
-                                else (pos, objects)
+move_object :: Pos -> [Pos] -> [Pos] -> [Pos] -> [Pos] -> Int -> Int -> Int -> (Pos, [Pos])
+move_object pos objects childs dirt robots i n m = if (in_range (get_surr_by_dir pos i objects) n m) && (notElem (get_surr_by_dir pos i objects) dirt) && (notElem (get_surr_by_dir pos i objects) robots)
+                                                    then ((mov_dir pos i), ( move_object__ (mov_dir pos i) (get_surr_by_dir pos i objects) objects))
+                                                    else (pos, objects)
 
--- current Pos, direction, childs list, dirt list, n, m -> (next Pos , newObj, newChild, newDirt)
-mv_child :: Pos -> Int -> [Child] -> [Pos] -> [Pos] -> Int -> Int -> (Pos, [Pos], [Child], [Pos])
-mv_child pos dir childs dirt robots objects n m = if (is_not_dirt pos dir dirt) && (is_not_bbppen pos dir babypen) && (is_not_robot pos dir robots) 
-                                                then (if is_obj pos dir objects
-                                                        then (mov_dir pos dir, ((snd (move_object pos objects childs dir n m)), (concat_list ((mov_dir pos dir), dir) (list_without_elems (pos, dir) childs))), dirt)
-                                                        else (mov_dir pos dir, objects, (concat_list ((mov_dir pos dir), dir) (list_without_elems (pos, dir) childs))), dirt)
-                                                    )
-                                                else (pos, objects, childs, dirt)
+-- movimiento del niño
+mov_child :: Pos -> [Pos] -> [Pos] -> [Pos] -> [Pos] -> [Pos] -> Int -> Int -> (Pos, [Pos], [Pos], [Pos])
+mov_child pos childs dirt robots objects babypen n m = mov_child__ pos (get_rand 0 3) childs dirt robots objects babypen n m (val_pos (not_in_list (not_in_list (not_in_list (not_in_list (get_surr_grid pos) dirt) robots) objects) babypen) n m) 
+
+mov_child__ :: Pos -> Int -> [Pos] -> [Pos] -> [Pos] -> [Pos] -> [Pos] -> Int -> Int -> [Pos] -> (Pos, [Pos], [Pos], [Pos])
+mov_child__ pos i childs dirt robots objects babypen n m grid = let next = mov_dir pos i
+                                                                in ( if in_range next n m && (notElem next childs) && (notElem next dirt) && (notElem next robots) && (notElem next babypen) 
+                                                                    then ( 
+                                                                        if (elem next objects)
+                                                                            then (let (mov, obj) = move_object pos objects childs dirt robots i n m
+                                                                                in (mov, obj, mov:(list_without_elems pos childs), dirt))
+                                                                            else ( next, objects, (next:(list_without_elems pos childs)), 
+                                                                                (if (get_rand 1 100) < 40 && (length (not_in_list (pos:(grid)) [next]) > 0)
+                                                                                    then (get_rand_elem_from_list 1 (not_in_list (pos:(grid)) [next]))++dirt
+                                                                                    else dirt)))
+                                                                    else (pos, objects, childs, (if (get_rand 1 100) < 20 && (length (not_in_list (pos:(grid)) [next]) > 0) 
+                                                                                                    then (get_rand_elem_from_list 1 grid)++dirt 
+                                                                                                    else dirt)))
 
 
 
+-- movimiento del robot
+mov_robot :: Pos -> [Pos] -> [Pos] -> [Pos] -> [Pos] -> [Pos] -> [Pos]  -> [Pos] -> Int -> Int -> (Pos, [Pos], [Pos], [Pos], [Pos], [Pos], [Pos])
+mov_robot pos childs dirt objects babypen babypenused robots robchi n m =  if (elem pos robchi)
+                                                                                then (if (elem pos babypen)
+                                                                                        then(pos, robots, [], childs, dirt, (list_without_elems pos babypen), (pos : babypenused))
+                                                                                        else ( let next = (breadth_first_search pos pos [] [[pos, pos]] babypen (objects++babypenused) n m)!!0
+                                                                                                in (if next == (-1,-1)
+                                                                                                        then (pos, robots, robchi, childs, dirt, babypen, babypenused)
+                                                                                                        else (next, [next], [next], childs, dirt, babypen, babypenused))))
+                                                                                else (  if (length childs) > 0 
+                                                                                            then (  if (elem pos childs)
+                                                                                                        then(pos, [pos], [pos], (list_without_elems pos childs), dirt, babypen, babypenused )
+                                                                                                        else(let next = (breadth_first_search pos pos [] [[pos, pos]] childs (objects++babypenused) n m)!!0
+                                                                                                                in (if next == (-1,-1)
+                                                                                                                    then (pos, robots, robchi, childs, dirt, babypen, babypenused)
+                                                                                                                    else (next, [next], robchi, childs, dirt, babypen, babypenused) ) ) )
+                                                                                            else ( if (length dirt) > 0
+                                                                                                    then (  if (elem pos dirt)
+                                                                                                                then (pos, robots, robchi, childs, (list_without_elems pos dirt), babypen, babypenused)
+                                                                                                                else (let next = (breadth_first_search pos pos [] [[pos, pos]] dirt (objects++babypenused) n m)!!0
+                                                                                                                        in (if next == (-1,-1)
+                                                                                                                            then (pos, robots, robchi, childs, dirt, babypen, babypenused)
+                                                                                                                            else (next, [next], robchi, childs, dirt, babypen, babypenused) ) ) )
+                                                                                                    else (pos, robots, robchi, childs, dirt, babypen, babypenused)))
+
+-- corral
+create_playpen :: Int -> Int -> Int -> Int  -> Pos -> [Pos]
+create_playpen dir i n m (x, y) | i == 0 || not (in_range (x, y) n m) = []
+                                | dir == 0 = (x, y) : create_playpen dir (i-1) n m (mov_dir (x, y) 1) 
+                                | otherwise = (x, y) : create_playpen dir (i-1) n m (mov_dir (x, y) 0)
--- utils.hs
+++ utils.hs
@@ -5,8 +5,8 @@ import System.Random
 type Pos = (Int, Int)
 type Child = (Pos, Int)
 
--------------------------------------------------------------------------------------
--------- METODOS AUXILIARES ---------------
+-------- METODOS AUXILIARES --------------
+
 -- generar un numero random en un intervalo
 get_rand :: Int -> Int -> Int 
 get_rand min max = unsafePerformIO (getStdRandom (randomR (min, max)))
@@ -27,6 +27,7 @@ not_in_list :: [Pos] -> [Pos] -> [Pos]
 not_in_list [] _ = []
 not_in_list (x:xr) list = if x `notElem` list 
                             then x : not_in_list xr list 
+                            else not_in_list xr list
  
 -- crea una lista con los primeros elementos de cada lista que contiene
 get_init :: [[Pos]] -> [Pos]
@@ -38,12 +39,58 @@ lwoe _ list [] = list
 lwoe i list (x:xr) = if i == x
                                     then list ++ xr
                                     else lwoe i (concat_list x list) xr
-list_without_elems i list = lwoe i [] list                           else not_in_list xr list
+list_without_elems i list = lwoe i [] list                           
 
--------------------------------------------------------------------------------------
------------ METODOS PARA EL BFS ------------
+
+get_rand_elem_from_list :: Int -> [Pos] -> [Pos]
+get_rand_elem_from_list i list = get_rand_elem_from_list__ 0 i (length list) list
+
+get_rand_elem_from_list__ :: Int -> Int -> Int -> [Pos] -> [Pos]
+get_rand_elem_from_list__ current n total list 
+        | current == n = []
+        | otherwise = let x = list!!(get_rand 0 (total - current - 1))
+                        in x : get_rand_elem_from_list__ (current + 1) n total (list_without_elems x list)
+
+
+get_surr_grid :: Pos -> [Pos]
+get_surr_grid (x, y) = [(x-1, y-1), (x-1, y), (x-1, y+1), (x, y-1), (x, y+1), (x+1, y-1), (x+1, y), (x+1, y+1)]
+
+
+
+
+
+
+
+
+
+
+create_matrix :: Int -> Int -> Int -> Int -> [Pos]
+create_matrix x y xt yt  | (x /= xt && y /= yt) = (x, y) : create_matrix x (y+1) xt yt
+                            |  y == yt = create_matrix (x + 1) 0 xt yt
+                            |  x == xt = [] 
+                            |  otherwise = []
+
+ 
+list_from_string :: [String] -> Int -> [String]
+list_from_string [] _ = []
+list_from_string list n = (unwords (take n list)) : list_from_string (drop n list) n
+
+
+print_string_list :: [String] -> IO ()
+print_string_list [] = putStr ""
+print_string_list (x:xr) = do 
+                    putStrLn x
+                    print_string_list xr
+
+print_field :: [Pos] -> [Pos] -> [Pos] -> [Pos] -> [Pos] -> [Pos] -> Int -> Int -> IO ()
+print_field robots dirt child objects babypen robotbaby n m = print_string_list (list_from_string (tail (create_list (0,0) (create_matrix 0 0 n m) robots dirt child objects babypen robotbaby)) m)
+
+
+
+----------- METODOS PARA EL breadth_first_search ------------
+
 -- returns if a position does not go off the board
-in_range (x, y) totalx totaly = x >= 0 && y >=0 && x < totalx && y < totaly
+in_range (x, y) xt yt = x >= 0 && y >=0 && x < xt && y < yt
 
 -- aledaños a una pos 
 mov_dir :: Pos -> Int -> Pos
@@ -66,11 +113,10 @@ looking_for_father ch (x:xr) | ch == (x!!0) = x
                         | otherwise = looking_for_father ch xr
 
 
--- buscar el mas cercano al inicio en el camino del bfs
+-- buscar el mas cercano al inicio en el camino del breadth_first_search
 find_closest :: Pos -> [Pos] -> [[Pos]] -> Pos
 find_closest destiny current elems | (current!!1) == destiny = current!!0
                                | otherwise = find_closest destiny (looking_for_father (current!!1) elems) elems
-
 
 rel_chi_father x y = concat_list y (concat_list x [])
 
@@ -79,13 +125,15 @@ asosiate_father :: [Pos] -> Pos -> [[Pos]]
 asosiate_father [] _ = []
 asosiate_father (x: xr) ch = (rel_chi_father x ch) : asosiate_father xr ch
 
--- bfs
-breadth_first_search :: Pos -> Pos -> [[Pos]] -> [[Pos]] -> [Pos] -> Int -> Int-> [Pos]
-breadth_first_search _ _ _ [] _ _ _ = [(-1,-1), (-1, -1)]
-breadth_first_search init pos seen queue elements n m  | pos `elem` elements =  concat_list pos (concat_list (find_closest init (head queue) seen) []) 
-                            | otherwise = breadth_first_search init 
-                                ((head ((tail queue)++(asosiate_father (not_in_list (val_pos (find_surrounding pos) n m) (get_init seen)) pos)))!!0) 
-                                (concat_list (head queue) seen) --
-                                ((tail queue)++(asosiate_father (not_in_list (val_pos (find_surrounding pos) n m) (get_init seen)) pos)) 
-                                elements
-                                n m
+-- BFS
+breadth_first_search :: Pos -> Pos -> [[Pos]] -> [[Pos]] -> [Pos] -> [Pos] -> Int -> Int-> [Pos]
+breadth_first_search _ _ _ [] _ _ _ _ = [(-1,-1), (-1, -1)]
+breadth_first_search init pos seen queue elements notemty n m  
+    | elem pos elements =  concat_list pos (concat_list (find_closest init (head queue) seen) []) 
+    | otherwise = breadth_first_search init 
+        ((head ((tail queue)++(asosiate_father (not_in_list (not_in_list (val_pos (find_surrounding pos) n m) (get_init seen)) notemty) pos)))!!0) 
+        ((head queue): seen) --
+        ((tail queue)++(asosiate_father (not_in_list (not_in_list (val_pos (find_surrounding pos) n m) (get_init seen)) notemty) pos)) 
+        elements
+        notemty
+        n m
